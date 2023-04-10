9cac8a1d97ed3c38129b167023785d3c3c3101aa
Teguh Muflih Rizky <teguhdieblau@gmail.com>
2023-04-10 22:05:13 +0700

    pengunaan state sederhana

--- app/src/main/java/com/teguhmuflih/myjetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/teguhmuflih/myjetpackcomposeapp/MainActivity.kt
@@ -3,27 +3,38 @@ package com.teguhmuflih.myjetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.teguhmuflih.myjetpackcomposeapp.ui.theme.MyJetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyJetpackComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            Column() {
+                Row(
+                    modifier = Modifier.padding(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Greeting("Android")
+                    val checkBoxState = remember { mutableStateOf(false) }
+                    Checkbox(checked = checkBoxState.value,
+                        onCheckedChange ={ checkBoxState.value = it} )
+                    Spacer(modifier = Modifier.padding(4.dp))
+                    Text(text = "Checkbox 1")
                 }
+
+
             }
         }
     }
